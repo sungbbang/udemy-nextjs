5a3852bfb5c8f7b44748a1ea9c3bb0844027547f
sungbbang <sinsa5133@gmail.com>
2026-02-04 19:29:21 +0900

    Navbar 컴포넌트 구현

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,7 @@
+import { LuUser } from 'react-icons/lu';
+
 function UserIcon() {
-  return <div>UserIcon</div>;
+  return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
 
 export default UserIcon;
