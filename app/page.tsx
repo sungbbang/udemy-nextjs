c8a92f48848d0a1233bb1e5b5e50435ea4d2d2e9
sungbbang <sinsa5133@gmail.com>
2026-02-04 17:06:05 +0900

    shadcn/ui 설치

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@/components/ui/button';
+
 function HomePage() {
-  return <h1 className='text-3xl'>HomePage</h1>;
+  return (
+    <div>
+      <h1 className='text-3xl'>HomePage</h1>
+      <Button variant='outline' size='lg' className='capitalize m-8'>
+        Click me
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
