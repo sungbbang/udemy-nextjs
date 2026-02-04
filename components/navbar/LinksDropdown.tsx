6cdfb6bfd4c95bf5c2a7e1afe3b398c1b9c4098f
sungbbang <sinsa5133@gmail.com>
2026-02-04 23:19:05 +0900

    LinksDropdown 컴포넌트 구현

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -2,6 +2,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { LuAlignLeft } from 'react-icons/lu';
@@ -9,6 +10,8 @@ import Link from 'next/link';
 import { Button } from '../ui/button';
 import UserIcon from './UserIcon';
 import { links } from '@/utils/links';
+import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
+import SignOutLink from './SignOutLink';
 
 function LinksDropdown() {
   return (
@@ -20,13 +23,32 @@ function LinksDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
-        {links.map(link => (
-          <DropdownMenuItem key={link.href}>
-            <Link href={link.href} className='capitalize w-full'>
-              {link.label}
-            </Link>
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal'>
+              <button className='w-full text-left'>Login</button>
+            </SignInButton>
           </DropdownMenuItem>
-        ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map(link => (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className='capitalize w-full'>
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
