ed75f72ba123b6a47f2c734bf8186e4235d9d284
sungbbang <sinsa5133@gmail.com>
2026-02-06 10:47:26 +0900

    프로필 모델 생성 및 Clerk 사용자 메타데이터 연동

--- utils/action.ts
+++ utils/action.ts
@@ -1,18 +1,41 @@
 'use server';
 
 import { profileSchema } from './schemas';
+import db from './db';
+import { clerkClient, currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData,
 ) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error('Please login to create a profile');
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: 'profile created' };
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+
+    const client = await clerkClient();
+    await client.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
-    console.log(error);
-    return { message: 'there was an error' };
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
   }
+
+  redirect('/');
 };
